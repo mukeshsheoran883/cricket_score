42aa8c5d7d411daff9254fc5005b5bff46f976a1
MukeshSheoran <mukeshsheoran883@gmail.com>
2023-11-08 18:18:54 +0530

    Home Screen commit

---
+++ lib/service/match_api_service.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'package:cricket_score/model/match_score_response.dart';
+import 'package:cricket_score/util/api_end_point.dart';
+import 'package:http/http.dart' as http;
+
+class MatchApiService{
+  static  Future<MatchResponse> getMatchInfo() async {
+    String url = ApiEndPoint.baseUrl;
+    http.Response response = await http.get(
+      Uri.parse(url),
+
+    );
+    if (response.statusCode == 200) {
+      String body = response.body;
+      final data = jsonDecode(body);
+      MatchResponse matchResponse = MatchResponse.fromJson(data);
+      return matchResponse;
+    } else {
+      throw 'Something went wrong';
+    }
+  }
+}
