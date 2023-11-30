118e57e1ad91069fc28cf3e8fb9bf3917d373afe
MukeshSheoran <mukeshsheoran883@gmail.com>
2023-11-30 14:43:59 +0530

    commit

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -27,7 +27,8 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   void initState() {
-    CricketProvider cricketProvider = Provider.of<CricketProvider>(context,listen: false);
+    CricketProvider cricketProvider =
+        Provider.of<CricketProvider>(context, listen: false);
     cricketProvider.loadScore();
     super.initState();
   }
@@ -35,141 +36,161 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Center(
-          child: Text('Cricket'),
+        appBar: AppBar(
+          title: const Center(
+            child: Text('Cricket'),
+          ),
         ),
-      ),
-      body:Consumer<CricketProvider>(builder: (BuildContext context, CricketProvider value, Widget? child) {
-       return
-        value.isLoading
-            ? const Center(
-                child: CircularProgressIndicator(),
-              )
-            : ListView.builder(
-                itemCount: value.matchResponse?.data?.length ?? 0,
-                itemBuilder: (context, index) {
-                  DataInfo data = value.matchResponse!.data![index];
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) {
-                        return MatchDetailScreen(
-                          information: value.matchResponse!.info,
-                          matchInfo: data,
-                        );
-                      }));
-                    },
-                    child: Card(
-                        child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(data.date.toString()),
-                          Text(data.name!.toString(), maxLines: 1),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Image.network(data.teamInfo?[0].img ?? '',height: 50,),
-                                  Text(data.teamInfo?[0].shortname ?? '',),
-                                ],
-                              ),
-                              Container(
-                                height: 30,
-                                width: 30,
-                                decoration: const BoxDecoration(
-                                    color: Colors.red, shape: BoxShape.circle),
-                                child: const Center(
-                                    child: Text(
-                                  'VS',
-                                  style: TextStyle(color: Colors.white),
-                                )),
-                              ),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Image.network(data.teamInfo?[1].img ?? '',height: 50,),
-                                  Text(data.teamInfo?[1].shortname ?? ''),
-                                ],
-                              ),
-                            ],
+        body: Consumer<CricketProvider>(
+          builder:
+              (BuildContext context, CricketProvider value, Widget? child) {
+            return value.isLoading
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : ListView.builder(
+                    itemCount: value.matchResponse?.data?.length ?? 0,
+                    itemBuilder: (context, index) {
+                      DataInfo data = value.matchResponse!.data![index];
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return MatchDetailScreen(
+                                  information: value.matchResponse!.info,
+                                  matchInfo: data,
+                                );
+                              },
+                            ),
+                          );
+                        },
+                        child: Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(data.date.toString()),
+                                Text(data.name!.toString(), maxLines: 1),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Image.network(
+                                          data.teamInfo?[0].img ?? '',
+                                          height: 50,
+                                        ),
+                                        Text(
+                                          data.teamInfo?[0].shortname ?? '',
+                                        ),
+                                      ],
+                                    ),
+                                    Container(
+                                      height: 30,
+                                      width: 30,
+                                      decoration: const BoxDecoration(
+                                          color: Colors.red,
+                                          shape: BoxShape.circle),
+                                      child: const Center(
+                                        child: Text(
+                                          'VS',
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                      ),
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Image.network(
+                                          data.teamInfo?[1].img ?? '',
+                                          height: 50,
+                                        ),
+                                        Text(data.teamInfo?[1].shortname ?? ''),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                                Text(data.status.toString()),
+                              ],
+                            ),
                           ),
-                          Text(data.status.toString()),
-                        ],
-                      ),
-                    )),
+                        ),
+                      );
+                    },
                   );
-                },
-              );
-      },)
+          },
+        )
 
-      // isLoading
-      //     ? const Center(
-      //         child: CircularProgressIndicator(),
-      //       )
-      //     : ListView.builder(
-      //         itemCount: matchResponse?.data?.length ?? 0,
-      //         itemBuilder: (context, index) {
-      //           DataInfo data = matchResponse!.data![index];
-      //           return GestureDetector(
-      //             onTap: () {
-      //               Navigator.push(context,
-      //                   MaterialPageRoute(builder: (context) {
-      //                 return MatchDetailScreen(
-      //                   information: matchResponse!.info,
-      //                   matchInfo: data,
-      //                 );
-      //               }));
-      //             },
-      //             child: Card(
-      //                 child: Padding(
-      //               padding: const EdgeInsets.all(16.0),
-      //               child: Column(
-      //                 mainAxisAlignment: MainAxisAlignment.center,
-      //                 children: [
-      //                   Text(data.date.toString()),
-      //                   Text(data.name!.toString(), maxLines: 1),
-      //                   Row(
-      //                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      //                     children: [
-      //                       Column(
-      //                         crossAxisAlignment: CrossAxisAlignment.start,
-      //                         children: [
-      //                           Image.network(data.teamInfo![0].img.toString()),
-      //                           Text(data.teamInfo![0].shortname.toString()),
-      //                         ],
-      //                       ),
-      //                       Container(
-      //                         height: 30,
-      //                         width: 30,
-      //                         decoration: const BoxDecoration(
-      //                             color: Colors.red, shape: BoxShape.circle),
-      //                         child: const Center(
-      //                             child: Text(
-      //                           'VS',
-      //                           style: TextStyle(color: Colors.white),
-      //                         )),
-      //                       ),
-      //                       Column(
-      //                         crossAxisAlignment: CrossAxisAlignment.start,
-      //                         children: [
-      //                           Image.network(data.teamInfo![1].img.toString()),
-      //                           Text(data.teamInfo![1].shortname.toString()),
-      //                         ],
-      //                       ),
-      //                     ],
-      //                   ),
-      //                   Text(data.status.toString()),
-      //                 ],
-      //               ),
-      //             )),
-      //           );
-      //         },
-      //       ),
-    );
+        // isLoading
+        //     ? const Center(
+        //         child: CircularProgressIndicator(),
+        //       )
+        //     : ListView.builder(
+        //         itemCount: matchResponse?.data?.length ?? 0,
+        //         itemBuilder: (context, index) {
+        //           DataInfo data = matchResponse!.data![index];
+        //           return GestureDetector(
+        //             onTap: () {
+        //               Navigator.push(context,
+        //                   MaterialPageRoute(builder: (context) {
+        //                 return MatchDetailScreen(
+        //                   information: matchResponse!.info,
+        //                   matchInfo: data,
+        //                 );
+        //               }));
+        //             },
+        //             child: Card(
+        //                 child: Padding(
+        //               padding: const EdgeInsets.all(16.0),
+        //               child: Column(
+        //                 mainAxisAlignment: MainAxisAlignment.center,
+        //                 children: [
+        //                   Text(data.date.toString()),
+        //                   Text(data.name!.toString(), maxLines: 1),
+        //                   Row(
+        //                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //                     children: [
+        //                       Column(
+        //                         crossAxisAlignment: CrossAxisAlignment.start,
+        //                         children: [
+        //                           Image.network(data.teamInfo![0].img.toString()),
+        //                           Text(data.teamInfo![0].shortname.toString()),
+        //                         ],
+        //                       ),
+        //                       Container(
+        //                         height: 30,
+        //                         width: 30,
+        //                         decoration: const BoxDecoration(
+        //                             color: Colors.red, shape: BoxShape.circle),
+        //                         child: const Center(
+        //                             child: Text(
+        //                           'VS',
+        //                           style: TextStyle(color: Colors.white),
+        //                         )),
+        //                       ),
+        //                       Column(
+        //                         crossAxisAlignment: CrossAxisAlignment.start,
+        //                         children: [
+        //                           Image.network(data.teamInfo![1].img.toString()),
+        //                           Text(data.teamInfo![1].shortname.toString()),
+        //                         ],
+        //                       ),
+        //                     ],
+        //                   ),
+        //                   Text(data.status.toString()),
+        //                 ],
+        //               ),
+        //             )),
+        //           );
+        //         },
+        //       ),
+        );
   }
 }
